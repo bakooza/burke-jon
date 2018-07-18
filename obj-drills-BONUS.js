4bab170add7761e9fbdaef24d92dd7e36ec69b41
Burkey Koontz <bk.koontz@gmail.com>
2018-07-18 15:55:08 -0700

    bonus question finished

--- obj-drills-BONUS.js
+++ obj-drills-BONUS.js
@@ -10,28 +10,37 @@ const HEROES = [
   { id: 7, name: 'Hulk', squad: 'Avengers' },
 ];
 
-/*function findOne(arr, query) {
-
-
-for (const key in query) {
-  if (query[key] === heroes[key])
-}
-}*/
-
 function findOne(heroesArray, objToCheck) {
-  const checkKeysArray = Object.keys(objToCheck);
-  const lengthOfCheckKeysArray = checkKeysArray.length;
-  let possibleHero = heroesArray.filter(element => element.id === objToCheck.id);
-  if (possibleHero === []) {
-    return false;
-  }
-  if (possibleHero)
+  // loop through heroesArray
+  // look for something in each element/object
+  // iterate through all the info in the hereoesArray object
+  // iterate through all the keys inside objToCheck
+  // if objtocheck key is also in heroesarray, good
   
-  )
- // filter the heroes array by whether or not id matches the id given in objtocheck
+//{ id: 1, name: 'Captain America', squad: 'Avengers' }
+  return heroesArray.find(heroObject =>  {
+    const oTCKeysArray = Object.keys(objToCheck);
+    const heroObjectKeysArray = Object.keys(heroObject);
+    let result = true;
+
+    oTCKeysArray.forEach(otckey => {
+      const match = heroObjectKeysArray.find(herokey => {
+        return herokey === otckey;
+      });
+      if (match !== undefined && heroObject[match] === objToCheck[match]) {
+        result = result && true; // if one of the iterations is false, they are all false
+      } else {
+        result = false;
+      }
+    });
+    return result;
+  }) || null;
+
+
 }
 
-findOne(HEROES, { id: 1 });
+
+console.log(findOne(HEROES, { squad: 'Justice League' }));
 // => { id: 1, name: 'Captain America', squad: 'Avengers' }
 
 // findOne(HEROES, { id: 10 });
@@ -45,3 +54,37 @@ findOne(HEROES, { id: 1 });
 
 // findOne(HEROES, { squad: 'Justice League' });
 // => { id: 4, name: 'Superman', squad: 'Justice League' }
+
+const Database = {
+  store: {
+    heroes: [
+      { id: 1, name: 'Captain America', squad: 'Avengers' },
+      { id: 2, name: 'Iron Man', squad: 'Avengers' },
+      { id: 3, name: 'Spiderman', squad: 'Avengers' },
+      { id: 4, name: 'Superman', squad: 'Justice League' },
+      { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+      { id: 6, name: 'Aquaman', squad: 'Justice League' },
+      { id: 7, name: 'Hulk', squad: 'Avengers' },
+    ]
+  },
+  findOne: function(objToCheck) {
+      return this.store.heroes.find(heroObject =>  {
+        const oTCKeysArray = Object.keys(objToCheck);
+        const heroObjectKeysArray = Object.keys(heroObject);
+        let result = true;
+        oTCKeysArray.forEach(otckey => {
+          const match = heroObjectKeysArray.find(herokey => {
+            return herokey === otckey;
+          });
+          if (match !== undefined && heroObject[match] === objToCheck[match]) {
+            result = result && true; // if one of the iterations is false, they are all false
+          } else {
+            result = false;
+          }
+        });
+        return result;
+      }) || null;
+  }
+};
+
+console.log(Database.findOne({ squad: 'Justice League' }));
