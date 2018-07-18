c2a760ad95b953686bb93a04f3e4f198fb1a7f68
jonsung89 <jonsung89@gmail.com>
2018-07-18 13:15:18 -0500

    Added validate-obj-key.js

---
+++ validate-obj-key.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function validateKeys(object, expectedKeys){
+  const objKeys = Object.keys(object);
+
+  if (objKeys.length !== expectedKeys.length) {
+    return false;
+  } else if {
+    
+  }
+}
