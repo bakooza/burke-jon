a114baa64486f6795050bedf306d1b7a04e5931d
Burkey Koontz <bk.koontz@gmail.com>
2018-07-18 11:17:06 -0700

    changed validate obj-keys

--- validate-obj-key.js
+++ validate-obj-key.js
@@ -9,3 +9,21 @@ function validateKeys(object, expectedKeys){
     
   }
 }
+
+
+function validateKeys(object, expectedKeys) {
+  const objKeys = Object.keys(object);
+  if (objKeys.length !== expectedKeys.length) {
+    return false;
+  }
+  let trueCounter = 0;
+  objKeys.forEach((element, index) => {
+    if (element === expectedKeys[index]) {
+      trueCounter++;
+    }
+  });
+  if (trueCounter === objKeys.length) {
+    return true;
+  }
+
+}
